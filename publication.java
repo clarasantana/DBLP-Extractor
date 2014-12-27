819b151df2ced5b0be6a2f4738a7be5dd4cbdbbc
Maria Clara Santana <claarasantana@icloud.com>
2014-12-26 21:58:52 -0300

    Create publication.java

---
+++ publication.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Publication {
+    private static Set ps= new HashSet(650000);
+    private static int maxNumberOfAuthors = 0;
+    private String key;
+    private Person[] authors;	// or editors
+
+    public Publication(String key, Person[] persons) {
+        this.key = key;
+        authors = persons;
+        ps.add(this);
+        if (persons.length > maxNumberOfAuthors)
+            maxNumberOfAuthors = persons.length;
+    }
+    
+    public static int getNumberOfPublications() {
+        return ps.size();
+    }
+    
+    public static int getMaxNumberOfAuthors() {
+        return maxNumberOfAuthors;
+    }
+    
+    public Person[] getAuthors() {
+        return authors;
+    }
+    
+    static Iterator iterator() {
+        return ps.iterator();
+    }
+}
